9703f6db06db43ca8b9e0d5cbd663e58a6c62239
Tejun Heo <htejun@fb.com>
2020-11-17 14:32:46 -0500

    util: finer grained interface for JsonArgsHelper
    Provide extra interface which breaks out saving part to a separate method.
    This will be used by resctl-bench.

--- util/src/json_file.rs
+++ util/src/json_file.rs
@@ -203,29 +203,43 @@ where
 
 pub trait JsonArgsHelper
 where
-    Self: JsonLoad + JsonSave + JsonArgs,
+    Self: JsonArgs,
 {
+    fn init_args_and_logging_nosave() -> Result<(JsonConfigFile<Self>, bool)>;
+    fn save_args(args_file: &JsonConfigFile<Self>) -> Result<()>;
     fn init_args_and_logging() -> Result<JsonConfigFile<Self>>;
 }
 
 impl<T> JsonArgsHelper for T
 where
-    T: JsonLoad + JsonSave + JsonArgs,
+    T: JsonArgs,
 {
-    fn init_args_and_logging() -> Result<JsonConfigFile<T>> {
+    fn init_args_and_logging_nosave() -> Result<(JsonConfigFile<T>, bool)> {
         let matches = T::match_cmdline();
         super::init_logging(T::verbosity(&matches));
 
         let mut args_file = JsonConfigFile::<T>::load_or_create(matches.value_of("args").as_ref())?;
+        let updated = args_file.data.process_cmdline(&matches);
 
-        if args_file.data.process_cmdline(&matches) && args_file.path.is_some() {
+        Ok((args_file, updated))
+    }
+
+    fn save_args(args_file: &JsonConfigFile<T>) -> Result<()> {
+        if args_file.path.is_some() {
             info!(
                 "Updating command line arguments file {:?}",
                 &args_file.path.as_deref().unwrap()
             );
             args_file.save()?;
         }
+        Ok(())
+    }
 
+    fn init_args_and_logging() -> Result<JsonConfigFile<T>> {
+        let (args_file, updated) = Self::init_args_and_logging_nosave()?;
+        if updated {
+            Self::save_args(&args_file)?;
+        }
         Ok(args_file)
     }
 }
