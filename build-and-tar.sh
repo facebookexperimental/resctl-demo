60c49e3bb6dada5e3e262f72aac39d5db3e3ad18
Tejun Heo <htejun@fb.com>
2021-01-13 14:50:52 -0500

    build-and-tar.sh: use plain cargo build
    cargo install was used to support musl target but musl binary turned out too
    slow to be useful. cargo install is slower and sometimes fails mysteriously.
    Revert to using plain cargo build.

--- build-and-tar.sh
+++ build-and-tar.sh
@@ -3,15 +3,12 @@
 set -e
 
 BINS=("rd-hashd" "rd-agent" "resctl-demo" "resctl-bench")
-INSTALL_ROOT="target/install-root"
-BUILT_BINS=("${BINS[@]/#/${INSTALL_ROOT}/bin/}")
+BUILT_BINS=("${BINS[@]/#/target/release/}")
 
 DIR=$(dirname "$0")
 cd "$DIR"
 
-for i in "${BINS[@]}"; do
-    cargo install --root ${INSTALL_ROOT} "${@}" --path $i
-done
+cargo build --release "${@}"
 
 echo "[ Creating target/resctl-demo.tar.gz ]"
 tar cvzf target/resctl-demo.tar.gz --transform 's|^.*/\([^/]*\)$|\1|' ${BUILT_BINS[@]}
