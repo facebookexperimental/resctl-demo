b5cf92806555921f9b88447fa2ce32d5763ea0d0
Tejun Heo <htejun@fb.com>
2021-02-04 13:34:31 -0500

    bench: iocost-tune: svg graph support added

--- resctl-bench/src/bench/iocost_tune/graph.rs
+++ resctl-bench/src/bench/iocost_tune/graph.rs
@@ -1,7 +1,7 @@
 use super::*;
 use plotlib::page::Page;
 use plotlib::repr::Plot;
-use plotlib::style::{PointMarker, PointStyle};
+use plotlib::style::{LineStyle, PointMarker, PointStyle};
 use plotlib::view::ContinuousView;
 
 pub struct Grapher<'a> {
@@ -39,27 +39,19 @@ impl<'a> Grapher<'a> {
             DataSel::Lat(_, _) => (0.0, 1000.0),
         };
 
-        let mut xlabel = format!("vrate: ");
         let lines = &series.lines;
-        xlabel += &if lines.low.0 > 0.0 {
-            format!(
-                "low-infl=({:.1}, {:.3}) ",
-                lines.low.0,
-                lines.low.1 * yscale
-            )
-        } else {
-            format!("min={:.3} ", lines.low.1 * yscale)
-        };
-        xlabel += &if lines.high.0 < vrate_max {
-            format!(
-                "high-infl=({:.1}, {:.3}) ",
-                lines.high.0,
-                lines.high.1 * yscale
-            )
-        } else {
-            format!("max={:.3} ", lines.high.1 * yscale)
-        };
-        xlabel += &format!("error={:.3}", series.error * yscale);
+        let mut xlabel = format!(
+            "vrate: min={:.3} max={:.3} ",
+            lines.low.1 * yscale,
+            lines.high.1 * yscale
+        );
+        if lines.low.0 > 0.0 {
+            xlabel += &format!("low-infl={:.1} ", lines.low.0);
+        }
+        if lines.high.0 < vrate_max {
+            xlabel += &format!("high-infl={:.1} ", lines.high.0);
+        }
+        xlabel += &format!("err={:.3}", series.error * yscale);
 
         let view = ContinuousView::new()
             .x_range(0.0, vrate_max * 1.1)
@@ -71,13 +63,12 @@ impl<'a> Grapher<'a> {
     }
 
     fn plot_one_text(&mut self, sel: &DataSel, series: &DataSeries) -> Result<()> {
-        const DIMENSION: (u32, u32) = (80, 24);
-
+        const SIZE: (u32, u32) = (80, 24);
         let (view, vrate_max, yscale) = Self::setup_view(sel, series);
 
         let mut lines = vec![];
-        for i in 0..DIMENSION.0 {
-            let vrate = vrate_max / DIMENSION.0 as f64 * i as f64;
+        for i in 0..SIZE.0 {
+            let vrate = vrate_max / SIZE.0 as f64 * i as f64;
             lines.push((vrate, series.lines.eval(vrate) * yscale));
         }
         let view =
@@ -91,14 +82,64 @@ impl<'a> Grapher<'a> {
         let view =
             view.add(Plot::new(points).point_style(PointStyle::new().marker(PointMarker::Circle)));
 
-        let page = Page::single(&view).dimensions(DIMENSION.0, DIMENSION.1);
+        let page = Page::single(&view).dimensions(SIZE.0, SIZE.1);
         write!(self.out, "{}\n\n", page.to_text().unwrap()).unwrap();
+        Ok(())
+    }
+
+    fn plot_filename(&self, sel: &DataSel) -> String {
+        format!("{}-{}.svg", self.file_prefix.as_ref().unwrap(), sel)
+    }
+
+    fn plot_one_svg(&mut self, sel: &DataSel, series: &DataSeries) -> Result<()> {
+        const SIZE: (u32, u32) = (640, 480);
+        let (view, vrate_max, yscale) = Self::setup_view(sel, series);
+
+        let lines = &series.lines;
+        let segments = vec![
+            (0.0, lines.low.1 * yscale),
+            (lines.low.0, lines.low.1 * yscale),
+            (lines.high.0, lines.high.1 * yscale),
+            (vrate_max, lines.high.1 * yscale),
+        ];
+        let view = view.add(Plot::new(segments).line_style(LineStyle::new().colour("#DD3355")));
+
+        let points = series
+            .points
+            .iter()
+            .map(|(vrate, val)| (*vrate, val * yscale))
+            .collect();
+        let view = view.add(
+            Plot::new(points).point_style(
+                PointStyle::new()
+                    .marker(PointMarker::Circle)
+                    .colour("#35C788"),
+            ),
+        );
+
+        let view = view.x_max_ticks(10).y_max_ticks(10);
+
+        if let Err(e) = Page::single(&view)
+            .dimensions(SIZE.0, SIZE.1)
+            .save(self.plot_filename(sel))
+        {
+            bail!("{}", &e);
+        }
         Ok(())
     }
 
     pub fn plot(&mut self, result: &IoCostTuneResult) -> Result<()> {
         for (sel, series) in result.data.iter() {
             self.plot_one_text(sel, series)?;
+            if self.file_prefix.is_some() {
+                if let Err(e) = self.plot_one_svg(sel, series) {
+                    warn!(
+                        "iocost-tune: Failed to plot graph into {:?} ({})",
+                        &self.plot_filename(sel),
+                        &e
+                    );
+                }
+            }
         }
         Ok(())
     }
