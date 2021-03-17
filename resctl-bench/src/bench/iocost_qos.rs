cb916230176f1ed53a431e0c19ac3e4f77f994ff
Tejun Heo <tj@kernel.org>
2021-03-16 19:30:51 -0700

    bench: iocost-qos: lower DFL_ISOL_PCT to 05

--- resctl-bench/src/bench/iocost_qos.rs
+++ resctl-bench/src/bench/iocost_qos.rs
@@ -13,7 +13,7 @@ const DFL_VRATE_MAX: f64 = 100.0;
 const DFL_VRATE_INTVS: u32 = 5;
 const DFL_STOR_BASE_LOOPS: u32 = 3;
 const DFL_STOR_LOOPS: u32 = 1;
-const DFL_ISOL_PCT: &'static str = "10";
+const DFL_ISOL_PCT: &'static str = "05";
 const DFL_ISOL_THR: f64 = 0.9;
 const DFL_RETRIES: u32 = 1;
 
