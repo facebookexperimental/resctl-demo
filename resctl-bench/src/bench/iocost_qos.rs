0dbc3832058515ae0f7c5405b0e4b50f8cfe9a41
Tejun Heo <htejun@fb.com>
2021-04-13 09:38:24 -0400

    bench: mem_hog_tune: target p01 isolation of 80% instead of p05 of 90%

--- resctl-bench/src/bench/iocost_qos.rs
+++ resctl-bench/src/bench/iocost_qos.rs
@@ -13,8 +13,8 @@ const DFL_VRATE_MAX: f64 = 100.0;
 const DFL_VRATE_INTVS: u32 = 5;
 const DFL_STOR_BASE_LOOPS: u32 = 3;
 const DFL_STOR_LOOPS: u32 = 1;
-const DFL_ISOL_PCT: &'static str = "05";
-const DFL_ISOL_THR: f64 = 0.9;
+const DFL_ISOL_PCT: &'static str = "01";
+const DFL_ISOL_THR: f64 = 0.8;
 const DFL_RETRIES: u32 = 1;
 
 // Don't go below 1% of the specified model when applying vrate-intvs.
