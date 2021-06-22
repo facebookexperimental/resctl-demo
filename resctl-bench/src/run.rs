dc93b193a4a724f161ab66916c5c93c90771325a
Tejun Heo <htejun@fb.com>
2021-06-22 16:05:27 -0400

    bench: don't let rctx.start_hashd_bench() clobber cmd.json
    There's no reason for starting a hashd benchmark to automatically clear the
    whole cmd.json. For each bench run, we're re-creating rctx and restarting
    rd-agent which involves resetting all agent files including cmd.json. We're
    already starting with a clean slate and the only thing clearing cmd.json
    again achieves is losing configurations specific to the run.
    
    This fixes zswap disable handling. While at it, add missing cmd_seq bumping
    when changing zswap_enabled.

--- resctl-bench/src/run.rs
+++ resctl-bench/src/run.rs
@@ -373,7 +373,11 @@ impl<'a, 'b> RunCtx<'a, 'b> {
 
     pub fn set_zswap_enabled(&mut self, enabled: Option<bool>) -> &mut Self {
         self.add_agent_init_fn(move |rctx| {
-            rctx.access_agent_files(|af| af.cmd.data.zswap_enabled = enabled)
+            rctx.access_agent_files(|af| {
+                af.cmd.data.cmd_seq += 1;
+                af.cmd.data.zswap_enabled = enabled;
+                af.cmd.save().unwrap();
+            })
         });
         self
     }
@@ -907,7 +911,6 @@ impl<'a, 'b> RunCtx<'a, 'b> {
         let mut next_seq = 0;
         self.access_agent_files(|af| {
             next_seq = af.bench.data.hashd_seq + 1;
-            af.cmd.data = Default::default();
             af.cmd.data.hashd[0].log_bps = log_bps.unwrap_or(dfl_params.log_bps);
             af.cmd.data.bench_hashd_balloon_size = self.base.balloon_size_hashd_bench();
             af.cmd.data.bench_hashd_args = extra_args;
